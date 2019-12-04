69056618c2a27021385045ea83b1a6efbecd34f3
Péter Siket-Szász <peter.siketszasz@gmail.com>
2019-12-04 21:58:01 +0200

    Added routing  and rough landing page layout 🏠

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import MenuButton from './Button'
 import Header from './Header'
+import Button from '@material-ui/core/Button'
 import { connect } from 'react-redux'
 import { changeUserName } from './redux/actions/userActions';
+import { faThumbsDown, faThumbsUp, faCog, faChartBar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 
@@ -31,13 +34,23 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <Header text={userName} />
-          <MenuButton data1={data1} handleClick={this.handleClick}></MenuButton>
+          <Button variant="contained" className="App-nav-button">
+            <span className="App-nav-button-text">My Data</span>
+            <FontAwesomeIcon icon={faChartBar} />
+          </Button>
+          <h1 className="App-title">Emotion tracking</h1>
+          <Button variant="contained" className="App-nav-button">
+            <span className="App-nav-button-text">Settings</span>
+            <FontAwesomeIcon icon={faCog} />
+          </Button>
         </header>
+        <main className="App-body">
+          <div className="App-body-title">How you feeling?</div>
+          <div className="App-feel-button-container">
+            <Button variant="contained" className="App-feel-button"><FontAwesomeIcon icon={faThumbsDown} color="red" size="3x"/></Button>
+            <Button variant="contained" className="App-feel-button"><FontAwesomeIcon icon={faThumbsUp} color="green" size="3x"/></Button>
+          </div>
+        </main>
       </div >
     )
   }
